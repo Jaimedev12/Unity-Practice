56dc457fccb10d704f7dcf26f03d45293592aeb8
Jaimedev12 <A00833173@itesm.mx>
2021-08-30 14:54:23 -0600

    Upload of Protoype 1

---
+++ Prototype 1/Assets/Scripts/MovimientoCarro.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCarro : MonoBehaviour
+{
+
+    [SerializeField] float velocidadFrente = 15f;
+    [SerializeField] float velocidadGiro = 10f;
+
+    float inputX;
+    float inputZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+
+        //Moverse adelante
+        transform.Translate(new Vector3(0, 0, velocidadFrente) * Time.deltaTime * inputZ);
+
+        //Girar a los lados
+        transform.Rotate(Vector3.up, Time.deltaTime * inputX * velocidadGiro);
+    }
+}
